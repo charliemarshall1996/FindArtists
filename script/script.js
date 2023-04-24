aef00a5b629c98b4abcf16b57081bf5e06b50317
Charlie Marshall <charlie.marshall1996@gmail.com>
2023-04-24 18:28:18 +0100

    Changes to script.js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
-
-//Fetch changelog
-fetch('CHANGELOG.md')
-  .then(response => response.text())
-  .then(text => {
-    document.getElementById('changeLog').innerHTML = marked.parse(text);
-  });
+  import fs from "fs";
+  import parseMD from 'parse-md';
   
+  // Get the changelog content
+  const changelog = fs.readFileSync('./CHANGELOG.md', 'utf8');
+  
+  // Parse the markdown
+  const {metadata, content} = parseMD(changelog);
+  
+  console.log(metadata)
+
+  // Display the changelog
+  document.getElementById('changeLog').innerHTML = content;
